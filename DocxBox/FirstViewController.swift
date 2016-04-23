c10cd72b32ea686302e2e23e1cc9b07b94c3f63c
Student <Student@howard20.istlabs.rit.edu>
2016-04-23 14:40:03 -0400

    Initial Commit

---
+++ DocxBox/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DocxBox
+//
+//  Created by Student on 4/23/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
